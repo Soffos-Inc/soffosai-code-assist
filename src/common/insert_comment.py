5400d85a395090ba86b9903502f2485f7beb432b
francosuch <franco.such@soffos.ai>
2023-11-20 17:43:01 +0800

    added spaces for the comment to be inserted to set_input_configs

--- src/common/insert_comment.py
+++ src/common/insert_comment.py
@@ -4,8 +4,9 @@ import os
 dir_path = r'soffosai_classes'
 
 # The comment to insert
+S = " "
 triple_quote = "'''"
-comment = triple_quote + "\n Before using a SoffosAIService into a SoffosPipeline, you must setup the service's input configuration.\n" + triple_quote
+comment = 8*S + triple_quote + f"\n{8*S}Before using a SoffosAIService into a SoffosPipeline, you must setup the service's input configuration.\n{8*S}" + triple_quote + '\n'
 
 # Iterate over all files in the directory
 for filename in os.listdir(dir_path):
